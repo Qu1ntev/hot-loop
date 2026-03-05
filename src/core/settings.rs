c1f20dbb8ccbb3260ad14475b361542aeb7d1516
Kirill <grebennikov534cv@gmail.com>
2026-03-05 20:03:53 +0300

    fix

--- src/core/settings.rs
+++ src/core/settings.rs
@@ -1,4 +1,68 @@
-mod settings;
-pub use settings::{
-    Settings, Seed
-};
+#[derive(Debug, Clone, Copy)]
+pub struct Settings {
+    pub sample_len: usize,
+    pub temperature: f64,
+    pub top_p: Option<f64>,
+    pub top_k: Option<usize>,
+    pub repeat_penalty: f32,
+    pub repeat_last_n: usize,
+    pub seed: Seed,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Seed {
+    // Random,
+    Custom(u64),
+    Default, // temporary
+}
+
+impl Default for Settings {
+    fn default() -> Self {
+        Self {
+            sample_len: 256,
+            temperature: 0.7,
+            top_p: None,
+            top_k: None,
+            repeat_penalty: 1.1,
+            repeat_last_n: 64,
+            seed: Seed::Default,
+        }
+    }
+}
+
+impl Settings {
+    pub fn with_sample_len(mut self, len: usize) -> Self {
+        self.sample_len = len;
+        self
+    }
+    
+    pub fn with_temperature(mut self, temperature: f64) -> Self {
+        self.temperature = temperature;
+        self
+    }
+    
+    pub fn with_top_p(mut self, top_p: Option<f64>) -> Self {
+        self.top_p = top_p;
+        self
+    }
+    
+    pub fn with_top_k(mut self, top_k: Option<usize>) -> Self {
+        self.top_k = top_k;
+        self
+    }
+    
+    pub fn with_repeat_penalty(mut self, repeat_penalty: f32) -> Self {
+        self.repeat_penalty = repeat_penalty;
+        self
+    }
+    
+    pub fn with_repeat_last_n(mut self, repeat_last_n: usize) -> Self {
+        self.repeat_last_n = repeat_last_n;
+        self
+    }
+    
+    pub fn with_seed(mut self, seed: Seed) -> Self {
+        self.seed = seed;
+        self
+    }
+}
