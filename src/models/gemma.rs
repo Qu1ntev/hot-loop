a73073338741818646960fe6d35b2f4ea9a66fae
Kirill <grebennikov534cv@gmail.com>
2026-03-16 14:35:53 +0300

    gemma working, no chat_tempate, bad speed

---
+++ src/models/gemma.rs
@@ -0,0 +1,4 @@
+pub mod gemma;
+mod transformers;
+mod chat_template;
+use chat_template::ChatTemplate;
---
+++ src/models/gemma/chat_template.rs
@@ -0,0 +1,40 @@
+//! # BETA!!! NOT WORKING!!!
+
+use tokenizers::Tokenizer;
+use crate::{Error, Role};
+
+#[derive(Clone)]
+pub(crate) struct ChatTemplate {
+    tokenizer: Tokenizer,
+}
+
+impl ChatTemplate {
+    pub fn new(
+        tokenizer: Tokenizer,
+    ) -> Result<Self, Error> {
+        Ok(Self { tokenizer })
+    }
+
+    pub fn tokenizer(&self) -> &Tokenizer {
+        &self.tokenizer
+    }
+
+    pub fn fmt_prompt(&self, prompt: &str, role: Role) -> Result<Vec<u32>, Error> {
+        let prompt = "\
+        <bos><start_of_turn>user\n\
+        Ты Умный Ассистент\n\n\
+        привет<end_of_turn>\n\
+        <start_of_turn>model\n\
+        ";
+        let tokens = self.tokenizer.encode(prompt, true)?;
+        Ok(tokens.get_ids().to_vec())
+    }
+
+    pub fn assistant_start_template(&self) -> Vec<u32> { // FIX!!! think mode
+        vec![]
+    }
+
+    pub fn eos_token(&self) -> u32 {
+        self.tokenizer.token_to_id("<end_of_turn>").unwrap_or(1)
+    }
+}
