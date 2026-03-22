375d034f99b038a101ecf47f784d1c33e5ba559f
Kirill <grebennikov534cv@gmail.com>
2026-03-22 13:02:40 +0300

    fix error

--- src/backend/error.rs
+++ src/backend/error.rs
@@ -11,20 +11,3 @@ pub enum Error {
     #[error("UnwrapNone: {0}")]
     UnwrapNone(String),
 }
-
-#[derive(Debug)]
-pub enum ErrorKind {
-    Candle,
-    Tokenizer,
-    UnwrapNone,
-}
-
-impl Error {
-    pub fn kind(&self) -> ErrorKind {
-        match self {
-            Error::Candle(_) => ErrorKind::Candle,
-            Error::Tokenizers(_) => ErrorKind::Tokenizer,
-            Error::UnwrapNone(_) => ErrorKind::UnwrapNone,
-        }
-    }
-}
