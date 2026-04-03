d16f62f9a6e2aa6ab2c4dcb28b1a8bd318f7a444
Kirill <grebennikov534cv@gmail.com>
2026-04-03 15:51:09 +0300

    remove unstable features

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 //! # High-Level Rust Crate for Running Gguf Chat-Models, Uses the Candle Backend
 
-#[cfg(feature = "mkl")]
-extern crate intel_mkl_src;
-
-#[cfg(feature = "accelerate")]
-extern crate accelerate_src;
+// #[cfg(feature = "mkl")]
+// extern crate intel_mkl_src;
+//
+// #[cfg(feature = "accelerate")]
+// extern crate accelerate_src;
 
 pub(crate) mod utils;
 pub use candle_core::Device;
